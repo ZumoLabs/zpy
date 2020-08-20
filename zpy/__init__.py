b8fd7e3d5d2f39611d5f2c9f4e2fee8096c54bbc
HugoCMU <hugo.p.cmu@gmail.com>
2020-08-20 16:29:25 -0700

    remove improt local

--- zpy/__init__.py
+++ zpy/__init__.py
@@ -1,12 +1,3 @@
-# TODO: Why does Blender require this manual importing?
-from . import blender
-from . import color
-from . import file
-from . import material
-from . import mesh
-from . import render
-from . import scene
-from . import image
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
