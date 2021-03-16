c37262583481cf9ba9a6e9e28e2214d8df601e4b
Norman Ponte <nponte@baymax.local>
2021-03-16 16:10:54 -0700

    fix some loop and other problems

--- run_test.sh
+++ run_test.sh
@@ -5,23 +5,24 @@ set -u
 set -o pipefail
 
 ZPY_VERSION=$(git describe --tags --dirty --always)
-DATASET_NAME="${test-$ZPY_VERSION}"
-DATASET_STATE="NONE"
+DATASET_NAME="test ${ZPY_VERSION} $(date '+%d/%m/%Y %H:%M:%S')"
 SCENE_NAME="test_full_v1"
 SLEEP_TIME=20
+DATASET_STATE="NONE"
+END_STATES=(READY GENERATING_FAILED)
 
 # CLI Login + Create the Test Dataset
 zpy login ${ZPY_USER} ${ZPY_PASS}
 zpy create dataset ${DATASET_NAME} ${SCENE_NAME}
 
 # Loop until failure or success
-while [ $DATASET_STATE != "GENERATING_FAILED" -o $DATASET_STATE == "READY"]; do
-DATASET_STATE=$(zpy list datasets | grep ${DATASET_NAME} | awk '{print $2}')
+while ! [[ ${END_STATES[*]} =~ "$DATASET_STATE" ]]; do
+DATASET_STATE=$(zpy list datasets | grep ${DATASET_NAME:0:20} | awk '{print $2}')
+echo "dataset '${DATASET_NAME}' :: ${DATASET_STATE}"
 sleep ${SLEEP_TIME}
 done
 
-if [$DATASET_STATE == "GENERATING_FAILED"]; do
+if [$DATASET_STATE == "GENERATING_FAILED"]; then
+echo "dataset '${DATASET_NAME}' generation failed"
 exit -1
-done
-
-exit 1
+fi
