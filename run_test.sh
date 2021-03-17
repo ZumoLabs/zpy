7926fcaa006ffdb15f893e8b103f4b061d14eead
Norman Ponte <nponte@baymax.local>
2021-03-17 14:23:57 -0700

    fix CA certs

--- run_test.sh
+++ run_test.sh
@@ -4,13 +4,18 @@ set -eo pipefail
 
 SCENE_NAME="test_full_v1"
 
+# apt setup
+apt-get update
+apt-get install
+
 # pip setup
-pip install /bender/zpy
+pip install -r requirements.txt
+pip install .
 
 # CLI Login + Create the Test Dataset
 zpy login ${ZPY_USER} ${ZPY_PASS}
 zpy get scene "$SCENE_NAME" .
-unzip "$SCENE_NAME"*
+unzip "${SCENE_NAME}*"
 
 /bin/blender-softwaregl --background --enable-autoexec --python launcher.py
 python launcher_helper.py --generate --scene_dir /bender
