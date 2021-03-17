06c48e2ce8a0d733a155fbee8c19ab999ca42027
Norman Ponte <nponte@baymax.local>
2021-03-17 16:22:51 -0700

    Fixes to add fake assets

--- run_test.sh
+++ run_test.sh
@@ -14,8 +14,13 @@ ${BLENDERPIP} install .
 
 # fetch scene
 mkdir /bender/scene
-aws s3 cp s3://zumo-labs/prod/ragnarok-scenes/$SCENE_ID.zip /bender/scene/test.zip
-unzip /bender/scene/test.zip
+aws s3 cp s3://zumo-labs/prod/ragnarok-scenes/$SCENE_ID.zip test.zip
+unzip test.zip -d /bender/scene
 
-/bin/blender-softwaregl --background --enable-autoexec --python launcher.py &
-python launcher_helper.py --generate --scene_dir /bender/scene
+# create fake assets dir
+export ASSETS="/tmp"
+cp /bender/scene/textures/test_hdri.hdr /tmp/lib/hdris/1k/test_hdri.hdr
+cp /bender/scene/textures/test_texture.jpg /tmp/lib/textures/random/test_texture.jpg
+
+/bin/blender-softwaregl --background --enable-autoexec --python /bender/launcher.py &
+${BLENDERPY} /bender/launcher_helper.py --generate --scene_dir /bender/scene
