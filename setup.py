66332e62952a9f29346435a6575cd48633222edf
Norman Ponte <nponte@baymax.local>
2021-03-17 15:07:25 -0700

    Fix requirements file

--- setup.py
+++ setup.py
@@ -5,11 +5,31 @@ from setuptools import find_packages
 import versioneer
 
 
-def get_requirements_from_file(filepath):
-    requires = []
-    with open(filepath, 'r') as f:
-        requires.append(f.readline())
-    return requires
+def get_requirements_from_file(python_requirements_file="./requirements.txt"):
+    """
+    Purpose:
+        Get python requirements from a specified requirements file.
+    Args:
+        python_requirements_file (String): Path to the requirements file (usually
+            it is requirements.txt in the same directory as the setup.py)
+    Return:
+        requirements (List of Strings): The python requirements necessary to run
+            the library
+    """
+
+    requirements = []
+    with open(python_requirements_file) as requirements_file:
+        requirement = requirements_file.readline()
+        while requirement:
+            if requirement.strip().startswith("#"):
+                pass
+            elif requirement.strip() == "":
+                pass
+            else:
+                requirements.append(requirement.strip())
+            requirement = requirements_file.readline()
+
+    return requirements
 
 
 def get_long_description(filepath):
@@ -29,7 +49,7 @@ setup(
     author_email='infra@zumolabs.ai',
     url='https://github.com/ZumoLabs/zpy',
     packages=find_packages(),
-    install_requires=get_requirements_from_file('requirements.txt'),
+    install_requires=get_requirements_from_file(),
     include_package_data=True,
     entry_points='[console_scripts]\nzpy=cli.cli:cli\n',
 )
