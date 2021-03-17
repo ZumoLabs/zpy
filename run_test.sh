3eb141ae7aa5beaf5f393ea724fb23712e1747dc
Norman Ponte <nponte@baymax.local>
2021-03-17 15:33:43 -0700

    test out background and aws cp

--- run_test.sh
+++ run_test.sh
@@ -2,20 +2,20 @@
 
 set -eo pipefail
 
-SCENE_NAME="test_full_v1"
+SCENE_ID="fac16daa-6c55-499e-aee6-dcf97ce75a43"
+ASSETS="/tmp"
 
 # apt setup
 apt-get update
-apt-get install
+apt-get install -qy awscli
 
 # pip setup
-${BLENDERPIP} install -r requirements.txt
 ${BLENDERPIP} install .
 
-# CLI Login + Create the Test Dataset
-zpy login ${ZPY_USER} ${ZPY_PASS}
-zpy get scene "$SCENE_NAME" .
-unzip "${SCENE_NAME}*"
+# fetch scene
+mkdir /bender/scene
+aws s3 cp s3://zumo-labs/prod/ragnarok-scenes/$SCENE_ID.zip /bender/scene/test.zip
+unzip /bender/scene/test.zip
 
-/bin/blender-softwaregl --background --enable-autoexec --python launcher.py
-python launcher_helper.py --generate --scene_dir /bender
+/bin/blender-softwaregl --background --enable-autoexec --python launcher.py &
+python launcher_helper.py --generate --scene_dir /bender/scene
