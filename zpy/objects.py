949cc5c1df150e7fbbdb9b436636251eef8fd241
kursad <kk.zumolabs.github@gmail.com>
2021-04-20 20:25:13 -0500

    set experimental vertex colors

--- zpy/objects.py
+++ zpy/objects.py
@@ -267,6 +267,7 @@ def segment(
         as_category (bool, optional): Segment as a category, if false will segment as instance. Defaults to False.
         as_single (bool, optional): Segment all child objects as well. Defaults to False.
     """
+    bpy.context.preferences.experimental.use_sculpt_vertex_colors = True
     obj = verify(obj)
     if color is None:
         color = zpy.color.random_color(output_style='frgb')
