bc55938d7f9bb009c7fd96a6bbce465a29a2d982
Norman Ponte <norman@zumolabs.ai>
2021-04-23 13:18:12 -0700

    cleaned up utils

--- cli/utils.py
+++ cli/utils.py
@@ -1,48 +1,97 @@
-from pathlib import Path
-from tqdm import tqdm
-from typing import Union
-from urllib.parse import urlparse
-from urllib.request import urlopen
 from cli.config import read_config
+from itertools import product
+from tqdm import tqdm
+from urllib.request import urlopen
 import functools
-import logging
-import os
-import random
-from cli.loader import Loader
-
-log = logging.getLogger(__name__)
 
 
-def parse_dataset_filter(dfilter):
-    """ parse a dataset filter string """
-    dfilter_arr = dfilter.split(':')
-    field, pattern, regex = 'name', 'startswith', dfilter_arr[-1]
-    if len(dfilter_arr) == 2:
-        pattern = dfilter_arr[0]
-    elif len(dfilter_arr) == 3:
-        field, pattern = dfilter_arr[0], dfilter_arr[1]
+def parse_filter(str_filter):
+    """ parse filter 
+
+    Parse filter string to field, pattern, regex.
+    Valid patterns depend on field but can include exact, startswith, contains, 
+    iexact, istartswith, icontains. If no pattern or field is provided the defaults
+    are name for field and startswith for pattern.
+
+    Examples:
+        icontains:hello
+        name:icontains:foobar
+        state:iexact:ready
+        foo
+
+    Args:
+        filter (str): filter query
+
+    Return:
+        field: field to filter on
+        pattern: pattern to apply regex
+        regex: string regex for pattern
+    """     
+    filter_arr = str_filter.split(':')
+    field, pattern, regex = 'name', 'startswith', filter_arr[-1]
+    if len(filter_arr) == 2:
+        pattern = filter_arr[0]
+    elif len(filter_arr) == 3:
+        field, pattern = filter_arr[0], filter_arr[1]
     return field, pattern, regex
 
 
-def _safe_eval(key):
-    try:
-        return eval(key)
-    except:
-        return key
+def resolve_sweep(sweep_config):
+    """ resolve sweep
+
+    Resolve a dictionary into a sweep of dictionaries. Gin bindings
+    are used to communicate with job code and therefore the sweep is done
+    across gin_bindings in the sweep_config.
+
+    Example:
+        {'foo': ['a', 'b']} -> {'foo': 'a'} {'foo': 'b'}
+
+    Args:
+        sweep_config (dict): dictionary to unfold into sweep
+    
+    Returns:
+        list: list of configs resolved from sweep config
+    """
+    configs, bindings = [], sweep_config['gin_bindings']
+    for random_binding in [dict(zip(bindings, v)) for v in product(*bindings.values())]:
+        config = deepcopy(sweep_config)
+        config['gin_bindings'] = random_binding
+        configs.append(config)
+    return configs
 
 
 def parse_args(args):
+    """ parse args
+
+    Used by cli to parse arguments passed to cli calls. Includes 
+    safe eval function to convert from string to other types.
+    
+    Example:
+        foo 1 bar 2 -> {'foo': 1, 'bar': 2}
+    """
+    def _safe_eval(key):
+        try:
+            return eval(key)
+        except:
+            return key
     keys = args[::2]
     vals = map(lambda x: _safe_eval(x), args[1::2])
     return dict(zip(keys,vals))
 
 
 def download_url(url, output_path):
+    """ download url
+
+    Download from url to give output path and visualize using tqdm.
+
+    Args:
+        url (str): url to download
+        output_path (str): path to download file to
+    """
     u = urlopen(url)
     h = u.info()
     totalSize = int(h["Content-Length"])
 
-    log.info(f"Downloading {totalSize} bytes...")
     fp = open(output_path, 'wb')
 
     blockSize = 8192
@@ -57,17 +106,12 @@ def download_url(url, output_path):
     fp.flush()
     fp.close()
 
-    log.info('Downloaded:')
-    log.info(f'url: {url}')
-    log.info(f'local path: {output_path}')
-
-
-def auth_headers(token):
-    return {'Authorization': 'token {}'.format(token)}
-
 
 def fetch_auth(func):
-    """ Decorator to fetch info for cli from config
+    """ fetch authetication
+
+    Decorator to wrap functions providing the backend url and the 
+    correct authorization headers for requests.
 
     Args:
         func: function to wrap
@@ -79,6 +123,6 @@ def fetch_auth(func):
     def wrapper(*args, **kwargs):
         config = read_config()
         endpoint = config['ENDPOINT']
-        auth_header = auth_headers(config['TOKEN'])
+        auth_header = {'Authorization': 'token {}'.format(config['TOKEN'])}
         return func(*args, **kwargs, url=endpoint, auth_headers=auth_header)
     return wrapper
